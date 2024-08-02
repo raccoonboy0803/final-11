389bdf0b74f6c1f0c97751bdf899e27e456398a2
raccoonboy <raccoonboy0803@gmail.com>
2024-08-03 03:26:46 +0900

    fix: callback수정

--- src/app/(providers)/(root)/(auth)/auth/callback/page.tsx
+++ src/app/(providers)/(root)/(auth)/auth/callback/page.tsx
@@ -1,3 +1,94 @@
+// 'use client';
+
+// import React, { useEffect } from 'react';
+// import { createClient } from '@/supabase/supabaseClient';
+// import { useRouter } from 'next/navigation';
+
+// function AuthCallback() {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const saveUserToDatabase = async (user: any) => {
+//       const userData = {
+//         // 구매자
+//         user_id: user.id,
+//         user_name: user.user_metadata.full_name,
+//         avatar_url: user.user_metadata.avatar_url,
+//         email: user.email,
+//         phone: '000-0000-0000',
+//         address: '',
+//         created_at: user.created_at,
+//         address_detail: '상세주소',
+//         address_code: '우편번호'
+//       };
+//       // const sellerData = {
+//       //   // 판매자
+//       //   seller_id: user.id,
+//       //   email: user.email,
+//       //   user_name: user.user_metadata.full_name,
+//       //   address: '',
+//       //   phone: '000-0000-0000',
+//       //   avatar_url: user.user_metadata.avatar_url
+//       // };
+
+//       try {
+//         const supabase = createClient();
+//         const { error } = await supabase.from('User').upsert([userData]);
+//         // const { error } = await supabase.from('Seller').upsert([sellerData]);
+
+//         if (error) {
+//           console.error('유저 정보를 데이터베이스에 저장하는 중 에러 발생:', error.message);
+//         } else {
+//           console.log('유저 정보를 데이터베이스에 성공적으로 저장');
+//         }
+//       } catch (error: any) {
+//         console.error('유저 정보를 데이터베이스에 저장하는 중 에러 발생:', error.message);
+//       }
+//     };
+
+//     // const checkUserExists = async (userId: string) => {
+//     //   const supabase = createClient();
+//     //   const { data, error } = await supabase
+//     //     .from('User')
+//     //     .select('user_id')
+//     //     .eq('user_id', userId)
+//     //     .single();
+
+//     //   if (error && error.code !== 'PGRST116') {
+//     //     // PGRST116 is the code for no rows found in supabase-js
+//     //     console.error('사용자 확인 중 에러 발생:', error.message);
+//     //     return false;
+//     //   }
+
+//     //   return data !== null;
+//     // };
+
+//     const getUserSession = async () => {
+//       const supabase = createClient();
+//       const { data } = await supabase.auth.getSession();
+//       return data;
+//     };
+
+//     const handleAuthCallback = async () => {
+//       const { session } = await getUserSession();
+//       if (session) {
+//         // const userExists = await checkUserExists(session.user.id);
+//         // if (!userExists) {
+//         //   await saveUserToDatabase(session.user);
+//         // }
+//         await saveUserToDatabase(session.user);
+//         router.push('/');
+//       }
+//     };
+
+//     handleAuthCallback();
+//   }, [router]);
+
+//   return <div>로그인 중...</div>;
+// }
+
+// export default AuthCallback;
+
 'use client';
 
 import React, { useEffect } from 'react';
@@ -21,47 +112,26 @@ function AuthCallback() {
         address_detail: '상세주소',
         address_code: '우편번호'
       };
-      const sellerData = {
-        // 판매자
-        seller_id: user.id,
-        email: user.email,
-        user_name: user.user_metadata.full_name,
-        address: '',
-        phone: '000-0000-0000',
-        avatar_url: user.user_metadata.avatar_url
-      };
 
       try {
         const supabase = createClient();
         const { error } = await supabase.from('User').upsert([userData]);
-        // const { error } = await supabase.from('Seller').upsert([sellerData]);
 
         if (error) {
           console.error('유저 정보를 데이터베이스에 저장하는 중 에러 발생:', error.message);
+          console.error('유저 정보를 데이터베이스에 저장하는 중 에러 발생:', error);
+          console.error('에러 세부 사항:', error.details);
+          console.error('힌트:', error.hint);
+          return false;
         } else {
           console.log('유저 정보를 데이터베이스에 성공적으로 저장');
+          return true;
         }
       } catch (error: any) {
         console.error('유저 정보를 데이터베이스에 저장하는 중 에러 발생:', error.message);
+        return false;
       }
     };
-
-    // const checkUserExists = async (userId: string) => {
-    //   const supabase = createClient();
-    //   const { data, error } = await supabase
-    //     .from('User')
-    //     .select('user_id')
-    //     .eq('user_id', userId)
-    //     .single();
-
-    //   if (error && error.code !== 'PGRST116') {
-    //     // PGRST116 is the code for no rows found in supabase-js
-    //     console.error('사용자 확인 중 에러 발생:', error.message);
-    //     return false;
-    //   }
-
-    //   return data !== null;
-    // };
 
     const getUserSession = async () => {
       const supabase = createClient();
@@ -72,12 +142,10 @@ function AuthCallback() {
     const handleAuthCallback = async () => {
       const { session } = await getUserSession();
       if (session) {
-        // const userExists = await checkUserExists(session.user.id);
-        // if (!userExists) {
-        //   await saveUserToDatabase(session.user);
-        // }
-        await saveUserToDatabase(session.user);
-        router.push('/');
+        const isUserSaved = await saveUserToDatabase(session.user);
+        if (isUserSaved) {
+          router.push('/');
+        }
       }
     };
 
